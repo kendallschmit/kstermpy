efeac190a22557d7c6baf4ac3d2705dbec4f48c5
Kendall Schmit <kendallschmit@icloud.com>
2020-11-10 03:17:44 +0000

    Many improvements
    Much better basic ANSI escape support. Cleaner logic in the Term class.

--- ktermpy.py
+++ ktermpy.py
@@ -11,13 +11,13 @@ import re
 import pty
 import time
 
-TERM = 'ktermpy'
+TERM = 'kstermpy'
 ENCODING = 'utf-8'
 POLL_FLAGS_READ = select.POLLIN | select.POLLPRI | select.POLLERR
 
-MODE_CHAR = 'CHAR'
-MODE_ESC = 'ESC'
-MODE_BRACKET = 'BRACKET'
+STATE_NORMAL = 'NORMAL'
+STATE_ESC = 'ESC'
+STATE_BRACKET = 'BRACKET'
 
 BELL = '\u0007'
 ANSI_ESC = '\u001b'
@@ -37,6 +37,26 @@ class TermClosed(Exception):
     pass
 
 
+def clamp_index(n, maximum):
+    return max(0, (min(n, maximum - 1)))
+
+
+def utf8read(f):
+    buf = f.read(1)
+    first = int(buf[0])
+    flag = 0x80
+    if first & flag:
+        remaining = 0
+        flag = flag >> 1
+        while first & flag:
+            flag = flag >> 1
+            remaining += 1
+        #eprint(f'UTF-8: size {remaining + 1}')
+        buf += f.read(remaining)
+    c = buf.decode(ENCODING)
+    return c
+
+
 class Term:
     def __init__(self, width=80, height=24):
         self.termf = None
@@ -46,10 +66,10 @@ class Term:
 
         self.width = width
         self.height = height
-        self.rows = [[' ' for col in range(self.width)] for row in range(self.height)]
 
-        self.charbuf = b''
         self.escbuf = ''
+
+        self.clear()
 
         pass
 
@@ -59,13 +79,12 @@ class Term:
 
         self.currow = 0
         self.curcol = 0
-        self.mode = MODE_CHAR
+        self.mode = STATE_NORMAL
 
         pid, fd = os.forkpty()
         if pid == 0:
             os.execle('/bin/login', '/bin/login', { 'TERM': TERM })
             os._exit()
-        eprint('child pid:', pid)
 
         self.termf = os.fdopen(fd, 'r+b', 0)
 
@@ -80,7 +99,6 @@ class Term:
     def send_input(self, buf):
         try:
             self.writepipe.write(buf)
-            eprint('in: ', buf, sep='')
         except Exception:
             raise TermClosed()
 
@@ -89,8 +107,8 @@ class Term:
         if self.done:
             raise TermClosed()
         info = {
-            'currow': self.currow,
-            'curcol': self.curcol,
+            'currow': clamp_index(self.currow, self.height),
+            'curcol': clamp_index(self.curcol, self.width),
             'mode': self.mode,
         }
         return copy.deepcopy(self.rows), info
@@ -117,17 +135,8 @@ class Term:
                     if event == select.POLLERR:
                         raise PollError('POLLERR event')
                     if fd == termfd:
-                        self.charbuf += self.termf.read(1)
-                        c = None
-                        try:
-                            c = self.charbuf.decode(ENCODING)
-                        except Exception:
-                            pass
-                        if c:
-                            self.handle_input(c)
-                            self.charbuf = b''
-                        elif len(self.charbuf) > 6:
-                            self.charbuf = b''
+                        c = utf8read(self.termf)
+                        self.handle(c)
                     elif fd == readpipefd:
                         b = self.readpipe.read(1)
                         self.termf.write(b)
@@ -136,12 +145,52 @@ class Term:
             self.readpipe.close()
 
 
+    def set_currow(self, n):
+        self.currow = clamp_index(n, self.height)
+
+
+    def move_currow(self, n):
+        self.set_currow(self.currow + n)
+
+
+    def set_curcol(self, n):
+        self.curcol = clamp_index(n, self.width)
+
+
+    def move_curcol(self, n):
+        self.set_curcol(self.curcol + n)
+
+
+    def newline(self):
+        if self.currow < self.height - 1:
+            self.move_currow(1)
+        else:
+            self.shift_rows()
+
+    def tab(self):
+        self.write_cell(' ')
+        self.curcol += 1
+        while self.curcol % TAB_WIDTH != 0:
+            self.write_cell(' ')
+            self.curcol += 1
+
+
+    def wrap(self):
+        self.newline()
+        self.curcol = 0
+
+
     def write_cell(self, cell):
-        if self.currow < 0 or self.currow >= self.height:
-            return
-        if self.curcol < 0 or self.curcol >= self.width:
-            return
+        if self.curcol >= self.width:
+            self.wrap()
         self.rows[self.currow][self.curcol] = cell
+        self.curcol += 1
+
+
+    def clear(self):
+        self.rows = [[' ' for col in range(self.width)] for row in range(self.height)]
+        self.currow = 0
+        self.curcol = 0
 
 
     def shift_rows(self):
@@ -149,65 +198,77 @@ class Term:
         self.rows.append([' ' for col in range(self.width)])
 
 
-    def handle_input(self, c):
-        if self.mode == MODE_CHAR:
-            self.handle_char(c)
-        elif self.mode == MODE_ESC:
+    def handle(self, c):
+        if self.mode == STATE_NORMAL:
+            self.handle_normal(c)
+        elif self.mode == STATE_ESC:
             self.handle_esc(c)
-        elif self.mode == MODE_BRACKET:
+        elif self.mode == STATE_BRACKET:
             self.handle_bracket(c)
 
 
-    def handle_char(self, c):
-        eprint('out:', c.encode(ENCODING), sep='')
-        if c == BELL:
+    def handle_normal(self, c):
+        if c == ANSI_ESC:
+            self.mode = STATE_ESC
+        elif c == BELL:
             pass
-        elif c == ANSI_ESC:
-            self.mode = MODE_ESC
         elif c == '\n':
-            if self.currow < self.height - 1:
-                self.currow += 1
-            else:
-                self.shift_rows()
+            self.newline()
         elif c == '\t':
-            self.write_cell(' ')
-            self.curcol += 1
-            while self.curcol % TAB_WIDTH != 0:
-                self.write_cell(' ')
-                self.curcol += 1
+            self.tab()
         elif c == '\r':
             self.curcol = 0
         elif c == '\b':
-            self.curcol -= 1
-            self.curcol = max(0, self.curcol)
+            self.move_curcol(-1)
         else:
             self.write_cell(c)
-            self.curcol += 1
 
 
     def handle_esc(self, c):
         if c == '[':
-            self.mode = MODE_BRACKET
+            self.mode = STATE_BRACKET
         else:
             self.write_cell(c)
-            self.mode = MODE_CHAR
+            self.mode = STATE_NORMAL
 
 
     def handle_bracket(self, c):
-        if c not in '[01234567890;=':
+        if c not in '01234567890;=':
             self.do_escbuf(c)
-            self.mode = MODE_CHAR
+            self.mode = STATE_NORMAL
         else:
             self.escbuf += c
 
 
     def do_escbuf(self, cmd):
-        eprint('escbuf:', self.escbuf)
-        if cmd == 'H':
+        eprint('ESC[', self.escbuf, cmd, end='\t')
+        args = self.escbuf.split(';')
+        if cmd == 'A':
+            n = (min(1, int(args[0])))
+            self.move_currow(-n)
+        elif cmd == 'B':
+            n = (min(1, int(args[0])))
+            self.move_currow(n)
+        elif cmd == 'C':
+            n = (min(1, int(args[0])))
+            self.move_curcol(n)
+        elif cmd == 'D':
+            n = (min(1, int(args[0])))
+            self.move_curcol(-n)
+        elif cmd == 'H' or cmd == 'f':
             try:
-                row, col = self.escbuf.split(';')
-                self.currow = int(row) - 1
-                self.curcol = int(col) - 1
-            except Exception as e:
-                eprint('Escape error:', e)
+                row, col = args
+                self.set_currow(int(row) - 1)
+                self.set_curcol(int(col) - 1)
+            except Exception:
+                self.currow = 0
+                self.curcol = 0
+        elif cmd == 'K':
+            for col in range(self.curcol, self.width):
+                if self.height > self.currow:
+                    self.rows[self.currow][col] = ' '
+        elif cmd == 'J':
+            self.clear()
+
+
         self.escbuf = ''
