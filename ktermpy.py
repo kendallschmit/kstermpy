3856b7a283b18d481285201dd4b8906bb102a24a
Kendall Schmit <kendallschmit@icloud.com>
2020-11-10 00:19:46 +0000

    Move pts server to own file

--- ktermpy.py
+++ ktermpy.py
@@ -6,8 +6,12 @@ import subprocess
 import copy
 import termios
 import tty
-import traceback
+import re
 
+import pty
+import time
+
+TERM = 'ktermpy'
 ENCODING = 'utf-8'
 POLL_FLAGS_READ = select.POLLIN | select.POLLPRI | select.POLLERR
 
@@ -15,13 +19,14 @@ MODE_CHAR = 'CHAR'
 MODE_ESC = 'ESC'
 MODE_BRACKET = 'BRACKET'
 
-BELL = b'\x07'
-ANSI_ESC = b'\x1b'
+BELL = '\u0007'
+ANSI_ESC = '\u001b'
 
 TAB_WIDTH = 8
 
 def eprint(*args, **kwargs):
-    print(*args, **kwargs, end='\r\n', file=sys.stderr)
+    print(*args, **kwargs, file=sys.stderr)
+    sys.stderr.flush()
 
 
 class PollError(Exception):
@@ -32,7 +37,7 @@ class TermClosed(Exception):
     pass
 
 
-class AnsiTerm:
+class Term:
     def __init__(self, width=80, height=24):
         self.termf = None
         self.readpipe = None
@@ -42,6 +47,9 @@ class AnsiTerm:
         self.width = width
         self.height = height
         self.rows = [[' ' for col in range(self.width)] for row in range(self.height)]
+
+        self.charbuf = b''
+        self.escbuf = ''
 
         pass
 
@@ -55,7 +63,9 @@ class AnsiTerm:
 
         pid, fd = os.forkpty()
         if pid == 0:
-            os.execl('/bin/login', '/bin/login')
+            os.execle('/bin/login', '/bin/login', { 'TERM': TERM })
+            os._exit()
+        eprint('child pid:', pid)
 
         self.termf = os.fdopen(fd, 'r+b', 0)
 
@@ -70,7 +80,7 @@ class AnsiTerm:
     def send_input(self, buf):
         try:
             self.writepipe.write(buf)
-            eprint('in: ', buf)
+            eprint('in: ', buf, sep='')
         except Exception:
             raise TermClosed()
 
@@ -107,8 +117,17 @@ class AnsiTerm:
                     if event == select.POLLERR:
                         raise PollError('POLLERR event')
                     if fd == termfd:
-                        b = self.termf.read(1)
-                        self.handle_byte(b)
+                        self.charbuf += self.termf.read(1)
+                        c = None
+                        try:
+                            c = self.charbuf.decode(ENCODING)
+                        except Exception:
+                            pass
+                        if c:
+                            self.handle_input(c)
+                            self.charbuf = b''
+                        elif len(self.charbuf) > 6:
+                            self.charbuf = b''
                     elif fd == readpipefd:
                         b = self.readpipe.read(1)
                         self.termf.write(b)
@@ -122,7 +141,7 @@ class AnsiTerm:
             return
         if self.curcol < 0 or self.curcol >= self.width:
             return
-        self.rows[self.currow][self.curcol] = cell.decode(ENCODING)
+        self.rows[self.currow][self.curcol] = cell
 
 
     def shift_rows(self):
@@ -130,107 +149,65 @@ class AnsiTerm:
         self.rows.append([' ' for col in range(self.width)])
 
 
-    def handle_byte(self, b):
+    def handle_input(self, c):
         if self.mode == MODE_CHAR:
-            self.handle_char(b)
+            self.handle_char(c)
         elif self.mode == MODE_ESC:
-            self.handle_esc(b)
+            self.handle_esc(c)
         elif self.mode == MODE_BRACKET:
-            self.handle_bracket(b)
+            self.handle_bracket(c)
 
 
-    def handle_char(self, b):
-        eprint('out:', b)
-        if b == BELL:
+    def handle_char(self, c):
+        eprint('out:', c.encode(ENCODING), sep='')
+        if c == BELL:
             pass
-        elif b == ANSI_ESC:
+        elif c == ANSI_ESC:
             self.mode = MODE_ESC
-        elif b == b'\n':
+        elif c == '\n':
             if self.currow < self.height - 1:
                 self.currow += 1
             else:
                 self.shift_rows()
-        elif b == b'\t':
-            self.write_cell(b' ')
+        elif c == '\t':
+            self.write_cell(' ')
             self.curcol += 1
-            while col % TAB_WIDTH != 0:
-                self.write_cell(b' ')
+            while self.curcol % TAB_WIDTH != 0:
+                self.write_cell(' ')
                 self.curcol += 1
-        elif b == b'\r':
+        elif c == '\r':
             self.curcol = 0
-        elif b == b'\b':
-            self.write_cell(b' ')
+        elif c == '\b':
             self.curcol -= 1
             self.curcol = max(0, self.curcol)
         else:
-            self.write_cell(b)
+            self.write_cell(c)
             self.curcol += 1
 
 
-    def handle_esc(self, b):
-        if b == b'[':
+    def handle_esc(self, c):
+        if c == '[':
             self.mode = MODE_BRACKET
         else:
-            self.write_cell(b)
+            self.write_cell(c)
             self.mode = MODE_CHAR
 
 
-    def handle_bracket(self, b):
-        if (b < b'0' or b > b'9') and b != b';' and b != b'=':
+    def handle_bracket(self, c):
+        if c not in '[01234567890;=':
+            self.do_escbuf(c)
             self.mode = MODE_CHAR
+        else:
+            self.escbuf += c
 
 
-def main():
-    stdinfd = sys.stdin.fileno()
-
-    try:
-        stdinattr = None
-        ansiterm = None
-        outf = None
-
-        stdinattr = termios.tcgetattr(stdinfd)
-        tty.setraw(stdinfd)
-        tty.setcbreak(stdinfd)
-
-        stdinfd = sys.stdin.fileno()
-        stdinf = os.fdopen(stdinfd, 'rb', 0)
-
-        ansiterm = AnsiTerm()
-        ansiterm.open()
-
-        outf = open('/tmp/aoeu', 'w')
-        def outprint(line):
-            outf.write(line + '\r\n')
-            outf.flush()
-
-        poller = select.poll()
-        poller.register(stdinf, select.POLLIN | select.POLLPRI | select.POLLERR)
-        while True:
-            polled = poller.poll(100)
-            for fd, event in polled:
-                if event == select.POLLERR:
-                    raise PollError('POLLERR event')
-                    return
-                if fd == stdinfd:
-                    b = stdinf.read(1)
-                    ansiterm.send_input(b)
-
-            outprint('\n\n\n')
-            rows, info = ansiterm.get_state()
-            outprint('TOP '.ljust(ansiterm.width + 4, '-'))
-            for i, r in enumerate(rows):
-                line = ''.join(r)
-                outprint(f'{i:2d} |{line}|')
-            outprint('BOT '.ljust(ansiterm.width + 4, '-'))
-            outprint(str(info) + '\n')
-    except TermClosed:
-        raise
-    finally:
-        if stdinattr:
-            termios.tcsetattr(stdinfd, termios.TCSAFLUSH, stdinattr)
-        if ansiterm:
-            ansiterm.close()
-
-
-if __name__ == '__main__':
-    main()
+    def do_escbuf(self, cmd):
+        eprint('escbuf:', self.escbuf)
+        if cmd == 'H':
+            try:
+                row, col = self.escbuf.split(';')
+                self.currow = int(row) - 1
+                self.curcol = int(col) - 1
+            except Exception as e:
+                eprint('Escape error:', e)
+        self.escbuf = ''
